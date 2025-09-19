aed24068862e03db01264746a7f2430eb1d19c24
Sujoy Adhikary <Sujoy.Adhikary@sonata-software.com>
2025-09-19 13:46:50 +0530

    update script

--- scripts/disable_auto_signing.rb
+++ scripts/disable_auto_signing.rb
@@ -3,31 +3,31 @@ require 'xcodeproj'
 # === Configuration ===
 project_path = 'Student/Student.xcodeproj'   
 
-# # === Load Project ===
-# project = Xcodeproj::Project.open(project_path)
+# === Load Project ===
+project = Xcodeproj::Project.open(project_path)
 
-# # === Target names to update ===
-# target_names = ['Student','SubmitAssignment', 'Widgets']
+# === Target names to update ===
+target_names = ['Student','SubmitAssignment', 'Widgets']
 
-# # === Process each target ===
-# target_names.each do |target_name|
-#   target = project.targets.find { |t| t.name == target_name }
+# === Process each target ===
+target_names.each do |target_name|
+  target = project.targets.find { |t| t.name == target_name }
 
-#   if target.nil?
-#     puts "❌ Target '#{target_name}' not found in project."
-#     next
-#   end
+  if target.nil?
+    puts "❌ Target '#{target_name}' not found in project."
+    next
+  end
 
-#   # Disable automatic signing
-#   target.build_configurations.each do |config|
-#     config.build_settings['CODE_SIGN_STYLE'] = 'Manual'
-#   end
+  # Disable automatic signing
+  target.build_configurations.each do |config|
+    config.build_settings['CODE_SIGN_STYLE'] = 'Manual'
+  end
 
-#   puts "✅ 'Automatically manage signing' disabled for target '#{target_name}'"
-# end
+  puts "✅ 'Automatically manage signing' disabled for target '#{target_name}'"
+end
 
-# # Save the project once after all changes
-# project.save
+# Save the project once after all changes
+project.save
 
 
 
